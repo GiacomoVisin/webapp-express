5a5052b9a9615a75bc67f0be54da7537a5434e2e
GiacomoVisin <killerboss1150@gmail.com>
2026-07-06 19:05:51 +0200

    creating functions for handling the errors

--- server.js
+++ server.js
@@ -2,10 +2,31 @@ const express = require(`express`)
 const app = express()
 const port = process.env.PORT || 3000
 
+app.listen(port, () => {
+    console.log(`The server is running on port http://localhost:${port}`)
+})
+
+
 app.get(`/`, (req, res) => {
     res.send(`Hello World!`)
 })
 
-app.listen(port, () => {
-    console.log(`The server is running on port http://localhost:${port}`)
-})
+
+function errorHandler(err, req, res, next) {
+    res.status(500)
+    res.json({ error: "Internal Server Error" })
+}
+
+
+function endpointNotFound(req, res, next){
+    res.status(404)
+    res.json({ error: "Endpoint not found" })
+}
+
+
+app.use(endpointNotFound)
+app.use(errorHandler)
+
+
+
+app.use(express.json())
