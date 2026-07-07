84f45621bfa54ddc761a29d2c57ba42580517ca5
GiacomoVisin <killerboss1150@gmail.com>
2026-07-07 22:01:12 +0200

    adding CORS for ajax request

--- server.js
+++ server.js
@@ -1,30 +1,28 @@
 const express = require(`express`)
+const cors = require('cors') 
 const app = express()
 const port = process.env.API_SERVER_PORT || 3000
 const router = require(`./router/routes`)
 
-app.listen(port, () => {
-    console.log(`The server is running on port http://localhost:${port}`)
-})
 
+app.use(cors()) 
 
+app.use(express.json())
+app.use(`/`, router)
 
 function errorHandler(err, req, res, next) {
     res.status(500)
     res.json({ error: "Internal Server Error" })
 }
 
-
 function endpointNotFound(req, res, next){
     res.status(404)
     res.json({ error: "Endpoint not found" })
 }
 
-
-
-app.use(express.json())
-app.use(`/`,router)
-
-
 app.use(endpointNotFound)
 app.use(errorHandler)
+
+app.listen(port, () => {
+    console.log(`The server is running on port http://localhost:${port}`)
+})
